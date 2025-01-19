1f7ee2f7fa8010895b758abc3e269383993738a7
Hamidou <hamiduguinea@gmail.com>
2025-01-19 23:45:54 +0100

    modified:   .gitignore 	new file:   firebase.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/add-note-dialog/add-note-dialog.component.html 	modified:   src/app/add-note-dialog/add-note-dialog.component.ts 	modified:   src/app/app.config.ts 	modified:   src/app/firebase-services/note-list.service.ts 	modified:   src/app/interfaces/note.interface.ts 	modified:   src/app/note-list/note-list.component.html 	modified:   src/app/note-list/note-list.component.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "note-e91fa", "appId": "1:731734529783:web:513a8013e5446db01b25f2", "storageBucket": "note-e91fa.firebasestorage.app", "apiKey": "AIzaSyDGHy6-xRzaeCqPPqpYHK3Zy7vF9UPWJWQ", "authDomain": "note-e91fa.firebaseapp.com", "messagingSenderId": "731734529783" })), provideFirestore(() => getFirestore())]
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,78 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, addDoc, collection, collectionData, doc, onSnapshot } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubNotes;
+  unsubTrash;
+
+
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
+  }
+
+  async addNote(item: Note) {
+    await addDoc(this.getNotesRef(), item).catch((err) => {
+      console.log(err);
+    }).then((docRef) => {
+      console.log('documnet written with id: ', docRef?.id);
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubNotes();
+    this.unsubTrash();
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
