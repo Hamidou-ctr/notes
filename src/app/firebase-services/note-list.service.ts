034b311493f5a0f20c1323485fbad971756f06bd
Hamidou <hamiduguinea@gmail.com>
2025-01-20 00:30:04 +0100

    implement note update functionality and enhance saveNote method

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { Firestore, addDoc, collection, collectionData, doc, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, doc, onSnapshot, updateDoc } from '@angular/fire/firestore';
 
 
 @Injectable({
@@ -22,6 +22,36 @@ export class NoteListService {
     this.unsubNotes = this.subNotesList();
     this.unsubTrash = this.subTrashList();
   }
+
+  async updateNote(note: Note) {
+    if(note.id){
+      let docRef = this.getSingleDocRef(this.getColIdFromNote(note), note.id);
+      await updateDoc(docRef, this.getCleanJson(note)).catch(
+        (err) => {
+          console.log('Error updating document: ', err);
+        }
+      );
+    }
+  }
+
+  getCleanJson(note: Note):{} {
+    return {
+      type: note.type,
+      title: note.title,
+      content: note.content,
+      marked: note.marked,
+    };
+  }
+
+  getColIdFromNote(note:Note): string {
+    if(note.type == 'note'){
+      return 'notes';
+    } else {
+      return 'trash';
+    }
+  }
+
+
 
   async addNote(item: Note) {
     await addDoc(this.getNotesRef(), item).catch((err) => {
