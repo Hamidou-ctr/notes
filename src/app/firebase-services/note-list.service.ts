f6021e31eccc701c380ab15ba904fe758a6b19db
Hamidou <hamiduguinea@gmail.com>
2025-01-20 20:59:50 +0100

    enhance note retrieval: add logging for note additions, modifications, and removals in subNotesList method

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -76,11 +76,29 @@ export class NoteListService {
   }
 
   subNotesList() {
+    //  wie man auf eine untercollection zugreift
+    //let ref = collection(this.firestore, 'trash/27I9mFTqUBKK1q6HrYqH/noteExtra');   
+    //const q = query(ref, limit(100));
+
+
     const q = query(this.getNotesRef(), limit(100));
     return onSnapshot(q, (list) => {
       this.normalNotes = [];
       list.forEach(element => {
         this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+
+
+      list.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          console.log("New note: ", change.doc.data());
+        }
+        if (change.type === "modified") {
+          console.log("Modified note: ", change.doc.data());
+        }
+        if (change.type === "removed") {
+          console.log("Removed note: ", change.doc.data());
+        }
       });
     });
   }
